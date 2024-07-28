a5f6ac17ab39ae93ed931804f77cb6a9dd810ce1
Marlon Santana <marlon.oliveira@somagrupo.com.br>
2024-07-28 14:38:10 -0300

    fix:  ajuste de  bugs layout e melhorias de layout

--- js/script.js
+++ js/script.js
@@ -33,15 +33,15 @@ const playButton = document.getElementById('playButton');
         document.addEventListener('DOMContentLoaded', function() {
     var accordions = document.querySelectorAll('.accordion-button');
 
-    accordions.forEach(function(accordion) {
-        accordion.addEventListener('click', function() {
-            this.classList.toggle('active');
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
-    });
+    // accordions.forEach(function(accordion) {
+    //     accordion.addEventListener('click', function() {
+    //         this.classList.toggle('active');
+    //         var content = this.nextElementSibling;
+    //         if (content.style.display === "block") {
+    //             content.style.display = "none";
+    //         } else {
+    //             content.style.display = "block";
+    //         }
+    //     });
+    // });
 });
